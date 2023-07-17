9a7922c4567da11ba9c4eb4cee6fed67a2ea7181
Darius Bost <dariusmb@ad.unc.edu>
2023-07-17 11:25:53 -0400

    Adjust for newly formatted data

--- neuralNet_bbox.py
+++ neuralNet_bbox.py
@@ -6,10 +6,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Load and preprocess the data
-dtypes = {'Query Strain ID': str, 'Array Strain ID': str, 'Genetic interaction score (ε)': float, 'P-value': float}
+dtypes = {'Query_allele': str, 'Array_allele': str, 'aggregated_growth_score': float, 'weighted_average_pvalue': float}
 chunksize = 100000  # Adjust the chunksize based on available memory and processing capabilities
 
-data_chunks = pd.read_csv("SGA_NxN_trimmed.txt", delimiter=',', dtype=dtypes, chunksize=chunksize)
+data_chunks = pd.read_csv("yeastRawData_StrainID_aggregateGrowth_weightedPval.csv", delimiter=',', dtype=dtypes, chunksize=chunksize)
 
 # Initialize an empty list to store chunks
 df_list = []
@@ -28,8 +28,8 @@ print("Number of columns:", num_columns)
 # Sample data
 # df = data.sample(n = 50)
 
-inputs = df[['Query Strain ID', 'Array Strain ID']]
-output = df['Genetic interaction score (ε)']
+inputs = df[['Query_allele', 'Array_allele']]
+output = df['aggregated_growth_score']
 
 # Convert gene pairs to binary vectors
 binary_mat = pd.get_dummies(inputs.astype(str), prefix='', prefix_sep='')
@@ -44,7 +44,7 @@ x_train, x_val, y_train, y_val = train_test_split(binary_inputs, output, test_si
 model = Sequential()
 
 # Add the first layer with input dimension
-model.add(Dense(132, activation='relu', input_dim=binary_inputs.shape[1]))
+model.add(Dense(500, activation='relu', input_dim=binary_inputs.shape[1]))
 
 # Add 10 hidden layers
 for _ in range(10):
