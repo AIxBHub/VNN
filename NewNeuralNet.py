433f317b6ae9b39a2e42b53b623ada5b9763aa9c
Darius Bost <dariusmb@ad.unc.edu>
2023-10-20 10:55:09 -0400

    Model build for ExE_NxE

--- NewNeuralNet.py
+++ NewNeuralNet.py
@@ -37,7 +37,7 @@ def create_binary_matrix(df, gene_col1, gene_col2):
 dtypes = {'Query_allele': str, 'Array_allele': str, 'aggregated_growth_score': float, 'weighted_average_pvalue': float}
 chunksize = 100000  # Adjust the chunksize based on available memory and processing capabilities
 
-data_chunks = pd.read_csv("ExE_yeastRawData_StrainID_aggregateGrowth_weightedPval.csv", delimiter=',', dtype=dtypes, chunksize=chunksize)
+data_chunks = pd.read_csv("ExE_NxE_yeastRawData_StrainID_aggregateGrowth_weightedPval.csv", delimiter=',', dtype=dtypes, chunksize=chunksize)
 
 # Initialize an empty list to store chunks
 df_list = []
@@ -62,7 +62,8 @@ df.memory_usage(deep=True).sum()
 # randomize sample observations
 df = df.sample(frac=1, random_state=42)
 
-# inputs = df[['Query_allele', 'Array_allele']]
+#gets first layer number of neurons based on total number of alleles
+neuron_nb = len(set(df['Query_allele']).union(set(df['Array_allele'])))
 
 # Call function to create binary matrix for input
 binary_inputs = create_binary_matrix(df, 'Query_allele', 'Array_allele')
@@ -78,7 +79,7 @@ x_train, x_val, y_train, y_val = train_test_split(binary_inputs, output, test_si
 model = Sequential()
 
 # Add the first layer with input dimension
-model.add(Dense(855, activation='relu', input_dim=binary_inputs.shape[1]))
+model.add(Dense(neuron_nb, activation='relu', input_dim=binary_inputs.shape[1]))
 
 # Add 10 hidden layers
 for _ in range(10):
@@ -113,14 +114,14 @@ with open('nnn_training_history.pkl', 'wb') as history_file:
     pickle.dump(history.history, history_file)
 
 # Save validation data to a CSV file
-x_val.to_csv('nnn_validation_x.csv', index=False)
-y_val.to_csv('nnn_validation_y.csv', index=False)
+x_val.to_csv('ExE_NxE/nnn_validation_x.csv', index=False)
+y_val.to_csv('ExE_NxE/nnn_validation_y.csv', index=False)
 
 # Evaluate the model
 score = model.evaluate(x_val, y_val)
 
 # Save model
-model.save('rawData_ExE_300epochs_batch1k_855neurons_12layers.h5')
+model.save('ExE_NxE/rawData_ExE_NxE_300epochs_batch1k_5471neurons_12layers.h5')
 
 # Plot the scores
 # plt.figure(figsize=(8, 6))
@@ -149,4 +150,4 @@ plt.plot(history.history['val_loss'], label='Validation Loss')
 plt.legend()
 plt.show()
 # Save the plot to a file
-plt.savefig('rawData_ExE_450bs_300Epoch_855neurons_12layers_plot.png')
+plt.savefig('ExE_NxE/rawData_ExE_NxE_1k_300Epoch_5471neurons_12layers_plot.png')
