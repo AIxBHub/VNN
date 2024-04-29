94bf879fc68eca937a1d150e72dad92cce6170fe
Darius Bost <dariusmb@ad.unc.edu>
2024-04-29 11:10:29 -0400

    Stremline builder
    grabs genes from ddot output file from James. Just give the GO terms as G_0

--- synthetic_yeast_dataset_builder.py
+++ synthetic_yeast_dataset_builder.py
@@ -4,28 +4,39 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from datetime import datetime
 import pandas as pd
+import os
+
+def extract_genes_for_root(file_path, root):
+    genes_set = set()
+    with open(file_path, 'r') as file:
+        root_found = False
+        for line in file:
+            line = line.strip()
+            if line.startswith("ROOT:") and line.split()[1] == root:
+                root_found = True
+            elif root_found and line.startswith("GENES:"):
+                genes_set.update(line.split()[1:])
+            elif root_found and line.startswith("ROOT:"):
+                break  # Exit loop if next root is encountered
+    return genes_set
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--GO_term', type=str, help='GO term for set P')
+args = parser.parse_args()
+# Example usage:
+# GO_0 = "GO:1905037"  # Change this to the desired root
+# file_path = "your_file.txt"  # Change this to the actual file path
+# genes = extract_genes_for_root(file_path, root)
+# print("Genes for root", root, ":", genes)
 
 # Step 1: Select top-level GO term
-G_0 = "GO:0006281"
+G_0 = "GO:0044237"
+file_path = os.path.join(os.getcwd(),"20240426_0044237_ontology_v1")
 
 # Step 2: Find subclasses of G_0 (set P)
 # Code to retrieve subclasses of G_0 from Gene Ontology database or any other source
 
-P = [
-    "YHR099W", "YDR540C", "YJL081C", "YDR448W", "YMR167W", "YOL133W", "YBR073W", "YMR173W", "YHR079C", "YML095C",
-    "YDL154W", "YKL045W", "YOL034W", "YJR043C", "YDR263C", "YPR175W", "YEL018W", "YGL090W", "YDR225W", "YPL164C",
-    "YHR154W", "YIL143C", "YER038C", "YDR446W", "YIL128W", "YDR288W", "YOR191W", "YPL235W", "YLR035C", "YOR023C",
-    "YDL140C", "YDL200C", "YDR190C", "YML021C", "YPR056W", "YOR386W", "YDR180W", "YER173W", "YLR383W", "YBR223C",
-    "YDR311W", "YGL201C", "YML023C", "YIL153W", "YBR010W", "YGR056W", "YJL168C", "YPL262W", "YPL204W", "YPL121C",
-    "YML011C", "YEL037C", "YFR038W", "YNL107W", "YCR014C", "YGL127C", "YOL043C", "YPL127C", "YNL215W", "YDR460W",
-    "YHR120W", "YOL094C", "YMR076C", "YFR037C", "YGL113W", "YOR368W", "YDL013W", "YFL024C", "YJL142C", "YPL046C",
-    "YPL122C", "YDR499W", "YJR068W", "YER147C", "YPL022W", "YLR005W", "YGR002C", "YNL102W", "YOR005C", "YDR079C",
-    "YGL163C", "YDR004W", "YGL168W", "YLR265C", "YNL290W", "YBR163W", "YKL067W", "YBR114W", "YDR334W", "YER162C",
-    "YDL164C", "YJR046W", "YEL032W", "YLL036C", "YOL095C", "YDR419W", "YPR164W", "YBL023C", "YEL019C", "YDL105W",
-    "YNL250W", "YMR114C", "YNL082W", "YOR328W", "YER095W", "YJL047C", "YER171W", "YLR320W", "YKL114C", "YJR006W",
-    "YJR082C", "YMR201C", "YJL065C", "YAL019W", "YLR274W", "YDR076W", "YAL015C", "YLR151C", "YBR087W", "YLR394W",
-    "YJR052W", "YOL087C", "YDR314C", "YBL003C", "YNL136W", "YDR363W"
-]
+P = extract_genes_for_root(file_path, G_0)
 
 # Step 3: Define parameters for the Gaussian distributions
 mu_f = 0  # mean for distribution f
@@ -33,7 +44,7 @@ sigma = 0.2  # standard deviation for both distributions
 delta = 1  # difference in mean between distributions f and g
 
 # Step 4: Read genes from file
-with open('20231219_final_gene_list.txt', 'r') as f:
+with open('../20231219_final_gene_list.txt', 'r') as f:
     all_genes = f.read().splitlines()
 
 # Step 5: Generate dataset
@@ -58,14 +69,14 @@ for _ in range(N):
     
     
 df = pd.DataFrame(dataset, columns=['Query_allele', 'Array_allele', 'Genetic_interaction_score','Distribution'])
-df_sub = pd.DataFrame(sub_dataset, columns=['Query_allele', 'Array_allele', 'Genetic_interaction_score','Distribution'])
+df_sub = pd.DataFrame(sub_dataset, columns=['Query_allele', 'Array_allele', 'Genetic_interaction_score'])
 
 current_datetime = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-filename = f"synthetic_yeast_data_{current_datetime}_{len(dataset)/1000000}mil.csv"
+filename = f"synthetic_yeast_data_{G_0}_{current_datetime}_{len(dataset)/1000000}mil.csv"
 df.to_csv(filename, index=False)
 df_label = f'Full Synthetic Dist N:{len(dataset)}'
 dfsub_label =  f'Full Synthetic Dist N:{len(sub_dataset)}'
-fig_file = f"Synthetic_Yeast_data_{len(dataset)/1000000}mil.png"
+fig_file = f"Synthetic_Yeast_data_{G_0}_{len(dataset)/1000000}mil.png"
 plt.figure(figsize=(15, 10))
 sns.kdeplot(df['Genetic_interaction_score'].values.flatten(), color='red', alpha = 0.5, label = df_label)
 sns.kdeplot(df_sub['Genetic_interaction_score'].values.flatten(), color='blue', alpha = 0.5, label = dfsub_label)
