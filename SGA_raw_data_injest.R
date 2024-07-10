7208c518feb8e7725aa0e6a77b37cc0e43a6c222
Darius Bost <dariusmb@ad.unc.edu>
2024-07-10 06:17:37 -0400

    Notes to run injest

--- SGA_raw_data_injest.R
+++ SGA_raw_data_injest.R
@@ -6,33 +6,26 @@ library(dplyr)
 
 # BiocManager::install("ctc")
 library(data.table)
+
 # Point to data source
 setwd("~/RENCI/CreativityHub_NNetworks")
-# s1 <- fread("Data File S1. Raw genetic interaction datasets: Pair-wise interaction format/SGA_DAmP.txt")
-# # 
-# s1_2 <- fread("Data File S1. Raw genetic interaction datasets: Pair-wise interaction format/SGA_ExE.txt")
-# # 
-# s1_3 <- fread("Data File S1. Raw genetic interaction datasets: Pair-wise interaction format/SGA_ExN_NxE.txt")
-# 
-# s1_4 <- fread("Data File S1. Raw genetic interaction datasets: Pair-wise interaction format/SGA_NxN.txt")
+# Home of the data
+# https://boonelab.ccbr.utoronto.ca/supplement/costanzo2009/
 
+# Read data in 
 s1 <- fread("sgadata_costanzo2009_rawdata_101120.txt")
 s2 <- fread("sgadata_costanzo2009_lenientCutoff_101120.txt")
+
+# Format headers to match nomeclature downstream
 new_names <- c("Query Strain ID","Query_gene","Array Strain ID","Array_gene","Genetic interaction score (ε)",
                "Standard_deviation","P-value","Query single mutant fitness (SMF)","Query SMF standard deviation",
                "Array SMF","Array SMF standard deviation","Double mutant fitness",
                "Double mutant fitness standard deviation")
 names(s1) <- new_names
+# Subset data to relevant columns
 dat <- s1[,c(1,3,5,7, 12)]
 
-
-# allData <- rbind(s1,s1_2,s1_3,s1_4)
-
-# tempDat <- allData
-# tempDat <- tempDat[,c(1,3,10,6,7)]
-# head(tempDat)
-# dat <- tempDat
-
+# Separate data into essential (temp-sensitive and DAmP) and non-essential gene data tables
 dat_allgenes <- na.omit(dat)
 dat_essent <- dat_allgenes[grep("_", dat_allgenes$`Query Strain ID`)] # all genes that are temp sensitive and Damp
 dat_nonEssent <- dat_allgenes[!grep("_", dat_allgenes$`Query Strain ID`)] # all other genes (most of which are deletion mutants)
@@ -41,10 +34,10 @@ dat_nonEssent2 <- dat_nonEssent2[!grep("-", dat_nonEssent2$`Query Strain ID`)]
 dat <- dat_nonEssent2
 
 
-dat$`Query Strain ID` <- gsub("_.*","",dat$`Query Strain ID`)
-dat$`Query Strain ID` <- gsub("-.*","",dat$`Query Strain ID`)
-dat$`Array Strain ID` <- gsub("_.*","",dat$`Array Strain ID`)
-dat$`Array Strain ID` <- gsub("-.*","",dat$`Array Strain ID`)
+# dat$`Query Strain ID` <- gsub("_.*","",dat$`Query Strain ID`)
+# dat$`Query Strain ID` <- gsub("-.*","",dat$`Query Strain ID`)
+# dat$`Array Strain ID` <- gsub("_.*","",dat$`Array Strain ID`)
+# dat$`Array Strain ID` <- gsub("-.*","",dat$`Array Strain ID`)
 
 #Check for repeating gene-pairs and how many
 dat$allele_pair <- paste(dat$`Query Strain ID`,dat$`Array Strain ID`,sep = "_")
@@ -56,29 +49,11 @@ dat <- dat %>%
   arrange(allele_pair, `P-value`) %>%
   distinct(allele_pair, .keep_all = TRUE)
 
-
-# Aggregate the growth score for each gene pair
-# aggregated_growth <- aggregate(dat$`Double mutant fitness`, by = list(dat$allele_pair), FUN = mean)
-# 
-# # Calculate the weights based on the p-values
-# weights <- 1 / dat$`P-value`
-
 # Add a small positive value to all p-values
 epsilon <- 1e-10
 adjusted_pvalues <- pmax(dat$`P-value`, epsilon)
 
-# Calculate the weighted average Pvalue for each gene pair
-# weighted_pvalue <- tapply(dat$`P-value`* adjusted_pvalues,
-#                           dat$allele_pair, sum) / tapply(adjusted_pvalues, 
-#                                                         dat$allele_pair, sum)
 
-# Create a new condensed data table with allele pairs, aggregated growth scores, and weighted average p-values
-# condensed_data <- data.table(
-#   allele_pair = unique(dat$allele_pair),
-#   aggregated_growth_score = aggregated_growth$x,
-#   weighted_eps_pvalue = weighted_pvalue,
-#   Genetic_interaction_score = (dat$`Genetic interaction score (ε)`)
-# )
 condensed_data <- data.table(
   Query_allele = dat$`Query Strain ID`,
   Array_allele = dat$`Array Strain ID`,
@@ -87,9 +62,7 @@ condensed_data <- data.table(
   Genetic_interaction_score = (dat$`Genetic interaction score (ε)`)
 )
 
-# condensed_data <- separate(condensed_data, allele_pair, 
-#                            into = c("Query_allele", "Array_allele"), sep = "_")
-
+# Locate preprocessed gene list and navigate there
 setwd("~/PGC_renci/dariusmb/AIxB/input_files_costanzo09/")
 
  # write.csv(condensed_data, "AllData_lowestPval_weightedPval.csv")
@@ -101,6 +74,7 @@ allele_list <- as.list(allele_list)
 percentages <- c(25, 50, 75, 100)
 
 # Iterate over each percentage value
+# This creates 4 datasets at varying percentages of the whole for testing purposes. Can just set percent to 100 to avoid extra data tables.
 for (percent in percentages) {
   # Set the percentage of genes to sample
   percentage_to_sample <- percent / 100
@@ -114,7 +88,7 @@ for (percent in percentages) {
     filter(Query_allele %in% sampled_genes & Array_allele %in% sampled_genes)
   
   # Write the sampled gene list to a file
-  write.csv(sampled_genes, paste0("ExE_Sampled_gene_list_", percent, "percent.txt"))
+  write.csv(sampled_genes, paste0("NxN_Sampled_gene_list_", percent, "percent.txt"))
   
-  write.csv(filtered_data, paste0("ExE_lowestPval_weightedPval_", percent, "percent.csv"))
+  write.csv(filtered_data, paste0("NxN_lowestPval_weightedPval_", percent, "percent.csv"))
 }
